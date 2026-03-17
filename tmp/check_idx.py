f8108eb2c8d355118d1abc8542ac83d2149cf245
tohshin <toshin.toshin1@gmail.com>
2026-03-18 01:26:39 +0900

    v1.0

--- tmp/check_idx.py
+++
@@ -1,8 +0,0 @@
-f_path = r"c:\Users\kyoui\tohshin_keiba\deploy_tmp\index.html"
-with open(f_path, 'r', encoding='utf-8', errors='ignore') as f:
-    content = f.read()
-    print(f"File length: {len(content)}")
-    if "fetchRaceResults" in content:
-        print("FOUND fetchRaceResults")
-    else:
-        print("NOT FOUND fetchRaceResults")
